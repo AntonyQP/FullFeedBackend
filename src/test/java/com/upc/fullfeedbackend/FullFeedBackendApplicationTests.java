410dae457d374c62917618a2a9b03200260c4375
Bryan Miramira <u201710061@upc.edu.pe>
2022-01-02 22:56:22 -0500

    feat: Project Initialization

---
+++ src/test/java/com/upc/fullfeedbackend/FullFeedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.fullfeedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullFeedBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
